148bf5948a6c1b21c44fb7309c69671d3d1fa905
4A-Dilla <ayofouad@gmail.com>
2021-01-18 13:48:43 -0500

    wrote threaded ssh bruteforcer

--- port_scanner/main.py
+++ port_scanner/main.py
@@ -9,11 +9,11 @@ def get_banner(soc):
 def scan_port(ipaddress, port):
     try:
         sock = socket.socket()
-        sock.settimeout(0.5)
+        sock.settimeout(1.0)
         sock.connect((ipaddress, port))
         try:
             banner = get_banner(sock)
-            print('[+] Open Port ' + str(port) + ' : ' + str(banner.decode().strip('\n')))
+            print('[+] Open Port ' + str(port) + ' : ' + str(banner.decode()))
         except:
             print('[+] Open Port ' + str(port))
     except:
@@ -31,7 +31,7 @@ def check_ip(ip):
 def scan(target):
     converted_ip = check_ip(target)
     print('\n' + '[...Scanning Target] ' + str(target))
-    for port in range(1, 100):
+    for port in range(1, 65535):
         scan_port(converted_ip, port)
 
 
